9e1458615fbc75d2eefdbcd8a7a4aacedb2cfc92
Christian Muise <christian.muise@gmail.com>
2020-04-17 23:05:32 -0400

    Initial setup.py and related scripts

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='python-utils',
+      version='0.1',
+      description='General library for setting up linux-based environments for developing, running, and evaluating planners.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url='https://github.com/AI-Planning/planning-utils',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=['planning-utils'],
+      scripts=['bin/planning-utils'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: Linux",
+      ],
+      python_requires='>=3.6',
+      zip_safe=False)

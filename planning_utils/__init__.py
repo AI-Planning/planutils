ed4bb73932bb9a93209d5f81c3c51dde821e543d
Christian Muise <christian.muise@gmail.com>
2020-04-27 22:40:04 -0400

    Improvements to the command line parsing.

--- planning_utils/__init__.py
+++ planning_utils/__init__.py
@@ -1,15 +1,16 @@
 
 import argparse
 
-from planning_utils.planner_installation import install_planner, install_planners
-
 
 def main():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--install_planners", help="install a collection of planners ('list' shows the options)")
+    parser.add_argument("-i", "--install",
+                        help="install an individual or collection of planners ('list' shows the options)",
+                        metavar="{planner or collection or list}")
     
     args = parser.parse_args()
 
-    if args.install_planners:
-        install_planners(args.install_planners)
+    if args.install:
+        from planning_utils.planner_installation import install
+        install(args.install)

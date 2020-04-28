1f56e2a58258bafc4c71f6cdfce6efd73c0e53ab
Christian Muise <christian.muise@gmail.com>
2020-04-28 00:06:53 -0400

    Installing the default scripts and getting the PATH working.

--- planning_utils/__init__.py
+++ planning_utils/__init__.py
@@ -3,15 +3,29 @@ import argparse, os
 
 
 def setup():
-    
-    assert not_setup_yet(), "Error: planning-utils is already setup."
 
-    print("Creating ~/.planning-utils...")
+    import stat
+    
+    assert not_setup_yet(), "Error: planning-utils is already setup. Remove ~/.planning-utils to reset (warning: all cached planners will be lost)."
+
+    print("\nCreating ~/.planning-utils...")
     os.mkdir(os.path.join(os.path.expanduser('~'), '.planning-utils'))
     os.mkdir(os.path.join(os.path.expanduser('~'), '.planning-utils', 'bin'))
 
-    print("Adding bin folder to path (assuming ~/.bashrc exists)...")
-    os.system('echo "export PATH=\\"$HOME/.planning-utils/bin:$PATH\\" >> ~/.bashrc')
+    print("\nAdding bin folder to path (assuming ~/.bashrc exists)...")
+    os.system('echo \'export PATH="$HOME/.planning-utils/bin:$PATH"\' >> ~/.bashrc')
+
+    print("\nInstalling planner scripts...")
+    from planning_utils.planner_installation import PLANNERS
+    for p in PLANNERS:
+        script  = "#!/bin/bash\n"
+        script += "echo 'Planner not installed! Run planning-utils --install %s\n" % p
+        with open(os.path.join(os.path.expanduser('~'), '.planning-utils', 'bin', p), 'w') as f:
+            f.write(script)
+        os.chmod(os.path.join(os.path.expanduser('~'), '.planning-utils', 'bin', p), stat.S_IEXEC)
+
+
+    print("\nAll set! Be sure to start a new bash session or update your PATH variable to include ~/.planning-utils/bin\n")
 
 def not_setup_yet():
     return not os.path.exists(os.path.join(os.path.expanduser('~'), '.planning-utils'))

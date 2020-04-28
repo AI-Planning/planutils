fce3b5370540f8dea67457fa39e55d11124a18fd
Christian Muise <christian.muise@gmail.com>
2020-04-27 23:23:27 -0400

    Add mandatory option to setup the utils (creating a local bin folder).

--- planning_utils/__init__.py
+++ planning_utils/__init__.py
@@ -1,6 +1,20 @@
 
-import argparse
+import argparse, os
 
+
+def setup():
+    
+    assert not_setup_yet(), "Error: planning-utils is already setup."
+
+    print("Creating ~/.planning-utils...")
+    os.mkdir(os.join('~', '.planning-utils'))
+    os.mkdir(os.join('~', '.planning-utils', 'bin'))
+
+    print("Adding bin folder to path (assuming ~/.bashrc exists)...")
+    os.system('echo "export PATH=\\"$HOME/.planning-utils/bin:$PATH\\" >> ~/.bashrc')
+
+def not_setup_yet():
+    return not os.path.exists(os.join('~', '.planning-utils'))
 
 def main():
     parser = argparse.ArgumentParser()
@@ -9,7 +23,15 @@ def main():
                         help="install an individual or collection of planners ('list' shows the options)",
                         metavar="{planner or collection or list}")
     
+    parser.add_argument("-s", "--setup", help="setup planning-utils for current user", action="store_true")
+    
     args = parser.parse_args()
+
+    if args.setup:
+        setup()
+    elif not_setup_yet():
+        print("Please run 'planning-utils --setup' before using utility.")
+        exit()
 
     if args.install:
         from planning_utils.planner_installation import install

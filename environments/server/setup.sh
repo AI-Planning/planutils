44b9b3339af8a61fc451a7e459b716843f71f67b
Christian Muise <christian.muise@gmail.com>
2022-05-31 12:10:53 -0400

    Easy setup for the server in docker.

---
+++ environments/server/setup.sh
@@ -0,0 +1,19 @@
+
+# Check if argument is build or run
+if [ "$1" = "build" ]; then
+    # Build
+    echo "Building..."
+    docker build -t planutils-server .
+    echo "Done."
+    exit 0
+elif [ "$1" = "run" ]; then
+    # Run
+    echo "Running..."
+    docker run -p 5555:5555 -d planutils-server
+    echo "Done."
+    exit 0
+else
+    # Print usage
+    echo "Usage: setup.sh build|run"
+    exit 1
+fi
